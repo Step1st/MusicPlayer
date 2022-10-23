e17f36c310c144da335d59d6bb4f7e55091dfc0b
Step1st <Step1st@users.noreply.github.com>
2022-10-23 20:50:02 +0300

    added progress bar and track label to playingview

--- MusicPlayerTerminalUI/Views/PlayingView.cs
+++ MusicPlayerTerminalUI/Views/PlayingView.cs
@@ -22,7 +22,28 @@ internal static class PlayingView
             Y = Pos.Percent(70f),
             Width = Dim.Percent(50f),
             Height = Dim.Percent(32f)
-        }; ;
+        };
+
+        var label = new Label("Track Name")
+        {
+            X = 0,
+            Y = Pos.Percent(0f),
+            Width = Dim.Fill(),
+            Height = 1
+        };
+
+        Playing.Add(label);
+
+        var progress = new ProgressBar()
+        {
+            X = 0,
+            Y = Pos.Percent(99f),
+            Width = Dim.Percent(50f),
+            Height = 1
+        };
+
+        Playing.Add(progress);
+
     }
 
 }
