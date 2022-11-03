89bfe0c7e66ba6b71b92e4f95df4322282dfee6a
Step1st <Step1st@users.noreply.github.com>
2022-11-03 12:59:14 +0200

    moved playback frame and add buttons

--- MusicPlayerTerminalUI/Views/MusicPlayerView.cs
+++ MusicPlayerTerminalUI/Views/MusicPlayerView.cs
@@ -33,19 +33,127 @@ internal class MusicPlayerView
             Border = new Border()
         };
 
-        Main.Add(TrackListView.TrackList);
-        Main.Add(PlaybackView.Playback);
-        Main.Add(PlayingView.Playing);
+        trackList = new FrameView("Track List")
+        {
+            X = 0,
+            Y = 0,
+            Width = Dim.Percent(85f),
+            Height = Dim.Percent(70f)
+        };
 
-        trackList = TrackListView.TrackList;
+        var list = new ListView(new List<string>() { "Track 1", "Track 2", "Track 3" })
+        {
+            X = 0,
+            Y = 0,
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+        
+        trackList.Add(list);
 
-        playback = PlaybackView.Playback;
+        playback = new FrameView("Playback")
+        {
+            X = Pos.Percent(85f),
+            Y = 0,
+            Width = Dim.Fill(),
+            Height = Dim.Percent(70f)
+        };
 
-        playing = PlayingView.Playing;
+        playback.Add(new Button("<< Seek")
+        {
+            X = Pos.Center(),
+            Y = Pos.Center()-3 - 3,
+            Width = 10,
+            Height = 1
+        });
+
+        // add playback controls
+        playback.Add(new Button("Play")
+        {
+            X = Pos.Center(),
+            Y = Pos.Center() -3 - 1,
+            Width = 10,
+            Height = 1
+        });
+
+        playback.Add(new Button("Pause")
+        {
+            X = Pos.Center(),
+            Y = Pos.Center() -3 + 1,
+            Width = 10,
+            Height = 1
+        });
+
+        playback.Add(new Button(">> Seek")
+        {
+            X = Pos.Center(),
+            Y = Pos.Center() -3 + 3,
+            Width = 10,
+            Height = 1
+        });
+
+        //add volume controls
+        playback.Add(new Button("+ Volume")
+        {
+            X = Pos.Center(),
+            Y = Pos.Center() +4 -1,
+            Width = 10,
+            Height = 1
+        });
+
+        playback.Add(new Button("- Volume")
+        {
+            X = Pos.Center(),
+            Y = Pos.Center() +4 + 1,
+            Width = 10,
+            Height = 1
+        });
+
+
+        playing = new FrameView("Playing")
+        {
+            X = 0,
+            Y = Pos.Percent(70f),
+            Width = Dim.Fill(),
+            Height = Dim.Percent(32f)
+        };
+
+        var label = new Label("Track Name")
+        {
+            X = 0,
+            Y = Pos.Percent(20f),
+            Width = Dim.Fill(),
+            Height = 1
+        };
+
+        playing.Add(label);
+
+        var progress = new ProgressBar()
+        {
+            X = 0,
+            Y = Pos.Percent(75f) - 1,
+            Width = Dim.Fill() - 13,
+            Height = 1,
+            ProgressBarFormat = ProgressBarFormat.Framed
+        };
+
+        playing.Add(progress);
+
+        var timelabel = new Label("00:00/00:00")
+        {
+            X = Pos.Right(progress) + 1,
+            Y = Pos.Percent(75f),
+            Width = Dim.Percent(50f),
+            Height = 1
+        };
+
+        playing.Add(timelabel);
 
         Main.Add(trackList);
         Main.Add(playback);
         Main.Add(playing);
+
+
         var top = Application.Top;
 
         var menu = new MenuBar(new MenuBarItem[]
