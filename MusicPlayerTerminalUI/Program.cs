d424fb875fe87dc52488b788f88b6dd960bb7033
Step1st <Step1st@users.noreply.github.com>
2022-11-04 12:29:16 +0200

    fixed compile bug

--- MusicPlayerTerminalUI/Program.cs
+++ MusicPlayerTerminalUI/Program.cs
@@ -15,4 +15,13 @@ internal class Program
 
 public class MockPlayer : IPlayer
 {
+    public void ChangeVolume(double volume) => throw new NotImplementedException();
+    public TimeSpan CurrentTime() => throw new NotImplementedException();
+    public void Next() => throw new NotImplementedException();
+    public void PlayPause() => throw new NotImplementedException();
+    public void Previous() => throw new NotImplementedException();
+    public void SeekBackward() => throw new NotImplementedException();
+    public void SeekForward() => throw new NotImplementedException();
+    public void Status() => throw new NotImplementedException();
+    public TimeSpan TotalTime() => throw new NotImplementedException();
 }
